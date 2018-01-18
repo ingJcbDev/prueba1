b4f6385a7b718812c1ae5bb06f0a73883288fede
ingJcbDev <ing.jcb.dev@gmail.com>
2018-01-18 13:49:32 -0500

    Agregue el archivo

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  
+<TABLE border="1">
+    <TR id="fila">
+        <TD><input type="button" value="oculta fila" onclick="document.getElementById('fila').style.visibility='hidden';" /></TD>
+        <TD>&nbsp; 11111111</TD>
+    </TR>
+    <TR>
+        <TD>&nbsp;</TD>
+        <TD>&nbsp;</TD>
+    </TR>
+</TABLE>  
+    
+    
+</html>
+
